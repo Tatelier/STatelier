374c4bd7e681b2c33b5a56b8979207c73b0dc21c
Pansystar <wh.bt3.p@gmail.com>
2021-05-31 08:47:47 +0900

    宣言順を変更

--- Tatelier.Nucleus/SongSelect/ItemControl.h
+++ Tatelier.Nucleus/SongSelect/ItemControl.h
@@ -10,13 +10,13 @@ class Item;
 
 namespace Tatelier::SongSelect {
 	class ItemControl {
-		std::string scoreFolder;
-		std::shared_ptr<std::vector<std::shared_ptr<Item>>> itemList;
-
 	public:
 		static ItemControl* Create(const std::string& folder, std::shared_ptr<CategoryControl> categoryControl);
 		std::shared_ptr<std::vector<std::shared_ptr<Item>>> GetItemList();
 		const std::string& GetScoreFolder();
 		ItemControl(const std::string& scoreFolder, std::shared_ptr<CategoryControl> categoryControl);
+	private:
+		std::string scoreFolder;
+		std::shared_ptr<std::vector<std::shared_ptr<Item>>> itemList;
 	};
 }
