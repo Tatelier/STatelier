92b8046cc3b4788826a9c6090513f2e6d3904cd4
Pansystar <wh.bt3.p@gmail.com>
2021-06-07 08:20:35 +0900

    hjsonexの返り値の型を修正

--- Tatelier.Nucleus/hjson/hjson-ex.h
+++ Tatelier.Nucleus/hjson/hjson-ex.h
@@ -1,35 +1,37 @@
 #pragma once
 
+#include <cstdint>
+
 #include <iostream>
 #include <sstream>
 
-namespace Hjson {
-	class Value;
-}
+#include "hjson.h"
 
 namespace HjsonEx {
 
+	constexpr int32_t SUCCESS = 1;
+
 	Hjson::Value Load(const std::string& path);
 
-	int EQv(const Hjson::Value& hj_value, const std::string& key, Hjson::Value* result);
-	int EQv(const Hjson::Value& hj_value, const std::string& key, Hjson::Value* result, const Hjson::Value& failure);
+	int32_t EQv(const Hjson::Value& hj_value, const std::string& key, Hjson::Value* result);
+	int32_t EQv(const Hjson::Value& hj_value, const std::string& key, Hjson::Value* result, const Hjson::Value& failure);
 
-	int EQa(const Hjson::Value& hj_value, const std::string& key, Hjson::Value* result);
-	int EQa(const Hjson::Value& hj_value, const std::string& key, Hjson::Value* result, const Hjson::Value& failure);
-
-	/**
-		 * @brief •¶š—ñ‚ğæ“¾
-		 */
-	int EQs(const Hjson::Value& hj_value, const std::string& key, std::string* result);
-	int EQs(const Hjson::Value& hj_value, const std::string& key, std::string* result, const std::string& failure);
+	int32_t EQa(const Hjson::Value& hj_value, const std::string& key, Hjson::Value* result);
+	int32_t EQa(const Hjson::Value& hj_value, const std::string& key, Hjson::Value* result, const Hjson::Value& failure);
 
 	/**
-		 * @brief int32Œ^‚Ì’l‚ğæ“¾ ¦¸”s‚Í0
-		 */
-	int EQi(const Hjson::Value& hj_value, const std::string& key, int32_t* result);
+	* @brief •¶š—ñ‚ğæ“¾
+	*/
+	int32_t EQs(const Hjson::Value& hj_value, const std::string& key, std::string* result);
+	int32_t EQs(const Hjson::Value& hj_value, const std::string& key, std::string* result, const std::string& failure);
 
 	/**
-		 * @brief int32Œ^‚Ì’l‚ğæ“¾
-		 */
-	int EQi(const Hjson::Value& hj_value, const std::string& key, int32_t* result, const int32_t& failure);
+	* @brief int32Œ^‚Ì’l‚ğæ“¾ ¦¸”s‚Í0
+	*/
+	int32_t EQi(const Hjson::Value& hj_value, const std::string& key, int32_t* result);
+
+	/**
+	* @brief int32Œ^‚Ì’l‚ğæ“¾
+	*/
+	int32_t EQi(const Hjson::Value& hj_value, const std::string& key, int32_t* result, const int32_t& failure);
 }
